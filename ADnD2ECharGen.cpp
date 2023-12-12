f48a961a073687e9e47e212dc6bf3c6cd7035b0c
DMStewart1981 <149893072+DMStewart1981@users.noreply.github.com>
2023-12-11 18:13:26 -0600

    Initial Upload
    Initial working program that rolls attributes (4d6 reroll 1s, take best 3).

---
+++ ADnD2ECharGen.cpp
@@ -0,0 +1,30 @@
+// ADnD2ECharGen.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "StatRolls.h"
+
+int main() {
+
+    auto Stats = StatList();
+
+    std::cout << "STR = " << Stats[0] << std::endl;
+    std::cout << "DEX = " << Stats[1] << std::endl;
+    std::cout << "CON = " << Stats[2] << std::endl;
+    std::cout << "INT = " << Stats[3] << std::endl;
+    std::cout << "WIS = " << Stats[4] << std::endl;
+    std::cout << "CHA = " << Stats[5] << std::endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
